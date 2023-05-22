d57a23810d81cd6966cad7f9af46cc23d35f5a4e
balatuluva <bala.tulva@gmail.com>
2023-05-22 20:27:55 +0100

    kms key

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello World"
+  key_id = "3e9cdae4-9ea5-4c02-92b5-37cb9963d48b"
 }
