bff75cd6abe368f96626fce03fada20cdec841d7
balatuluva <bala.tulva@gmail.com>
2023-05-22 21:46:01 +0100

    aws parameters

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue.gehana26.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart.gehana26.online:8080/" }
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "SecureString"
-  value = "Hello World"
+  count = length(var.parameters)
+  name  = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "3e9cdae4-9ea5-4c02-92b5-37cb9963d48b"
 }
