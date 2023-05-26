b244b60131502df27436e2bf98a79f79af2d8f05
balatuluva <bala.tulva@gmail.com>
2023-05-26 07:13:27 +0100

    secure string for passwords

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_ssm_parameter" "parameter" {
   type  = "String"
   key_id = "3e9cdae4-9ea5-4c02-92b5-37cb9963d48b"
 }
+
+# passwords should never keep inside code,
+resource "aws_ssm_parameter" "passwords" {
+  count = length(var.passwords)
+  name  = var.passwords[count.index].name
+  value = var.passwords[count.index].value
+  type  = "SecureString"
+  key_id = "3e9cdae4-9ea5-4c02-92b5-37cb9963d48b"
+}
